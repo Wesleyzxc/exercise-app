0e0da5a29f67942820fd6a0bb03fbbf891e5051d
Wesleyzxc <wesleykok-@hotmail.com>
2022-12-18 15:53:34 +1000

    dafsdf

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,17 +1,20 @@
 import { DataSource } from 'typeorm';
 
+const dataSource = new DataSource({
+  type: 'sqlite',
+  database: 'test',
+  entities: ['src/database/**/*.entity.{ts,js}'],
+  migrations: ['src/migrations/**/*.{ts,js}'],
+  synchronize: true, // shouldnt be used in prod
+});
+
 export const databaseProviders = [
   {
     provide: 'DATA_SOURCE',
     useFactory: async () => {
-      const dataSource = new DataSource({
-        type: 'sqlite',
-        database: 'test',
-        entities: [__dirname + '/../**/*.entity.ts'],
-        synchronize: true, // shouldnt be used in prod
-      });
-
       return dataSource.initialize();
     },
   },
 ];
+
+export default dataSource;
